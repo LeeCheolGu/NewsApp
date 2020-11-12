546774e3ea703c5d32549efa7e159a42760faa88
LeeCheolGu <cjfrn0443@gmail.com>
2020-11-12 17:31:25 +0900

    first commit

---
+++ newsTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newsTable
+//
+//  Created by 이철구 on 2020/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
