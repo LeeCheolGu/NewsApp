898f9435c9943b4dadef76db367b1b1e9bd6a161
LeeCheolGu <cjfrn0443@gmail.com>
2020-11-13 17:50:53 +0900

    Connected News API

--- newsTable/Controllers/ViewController.swift
+++ newsTable/Controllers/ViewController.swift
@@ -11,16 +11,56 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     @IBOutlet weak var tableViewMain: UITableView!
     
+    var newsData : Array<Dictionary<String, Any>>?
+    
+    func getNews() {
+        let task = URLSession.shared.dataTask(with: URL(string: "https://newsapi.org/v2/top-headlines?country=kr&apiKey=Your Key")!) { (data, response, error) in
+            if let dataJson = data {
+                //print(newsData)
+                do {
+                    let json = try JSONSerialization.jsonObject(with: dataJson, options: []) as! Dictionary<String, Any>
+                    print(json)
+                    let articles = json["articles"] as! Array<Dictionary<String,Any>>
+                    self.newsData = articles
+                    DispatchQueue.main.async {
+                        self.tableViewMain.reloadData()
+                    }
+//                    for (idx, value) in articles.enumerated() {
+//                        if let v = value as? Dictionary<String, Any> {
+//
+//                        }
+//
+//                    }
+                } catch {
+                    print("Error!! \(error)")
+                }
+            }
+        }
+        task.resume()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //몇개?
-        return 10
+        if let news = newsData {
+            return news.count
+        } else {
+            return 0
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //무엇?
         //let cell = UITableViewCell.init(style: .default, reuseIdentifier: "TableCellType1")
+        let idx = indexPath.row
         let cell = tableViewMain.dequeueReusableCell(withIdentifier: "Type1", for: indexPath) as! Type1
-        cell.labelText?.text = "\(indexPath.row)"
+        if let news = newsData {
+            let row = news[idx]
+            if let r = row as? Dictionary<String, Any> {
+                if let title = r["title"] as? String {
+                    cell.labelText?.text = title
+                }
+            }
+        }
         return cell
     }
     
@@ -32,6 +72,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.viewDidLoad()
         tableViewMain.delegate = self
         tableViewMain.dataSource = self
+        getNews()
     }
  
     //1. 데이터 무엇?
